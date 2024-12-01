6a8c8b43c415114a51b7699c487b0b61b025e46c
smartdriver321 <tinggaldidisneyland@gmail.com>
2024-12-01 14:34:38 +0700

    Initial commit

---
+++ frontend/src/main.tsx
@@ -0,0 +1,27 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
+import { ClerkProvider } from '@clerk/clerk-react'
+
+import './index.css'
+import App from './App.tsx'
+import AuthProvider from './providers/AuthProvider.tsx'
+
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
+console.log(PUBLISHABLE_KEY)
+
+if (!PUBLISHABLE_KEY) {
+	throw new Error('Missing Publishable Key')
+}
+
+createRoot(document.getElementById('root')!).render(
+	<StrictMode>
+		<ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl='/'>
+			<AuthProvider>
+				<BrowserRouter>
+					<App />
+				</BrowserRouter>
+			</AuthProvider>
+		</ClerkProvider>
+	</StrictMode>
+)
